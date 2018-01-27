0893c4b8c49d892eb8245a69a2512474bc18bd77
Skyler Hektner <skylerhektner@gmail.com>
2018-01-26 20:12:27 -0700

    Created basics for HUD

---
+++ Assets/Scripts/UI/HudManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudManager : MonoBehaviour {
+
+    // Singleton
+    public static HudManager instance;
+
+    // Privates
+    Slider DeathSlider;
+
+    void Start()
+    {
+        instance = this;
+
+        // create connections with private
+        DeathSlider = transform.Find("DeathTimer").Find("DeathSlider").GetComponent<Slider>();
+    }
+
+    public void UpdateDeathSlider(int newValue)
+    {
+        if (newValue > 1 || newValue < 0)
+        {
+            Debug.Log("You tried to set the DeathSlider to an impossible value: " + newValue);
+            return;
+        }
+
+        DeathSlider.value = newValue;
+    }
+
+
+}

f97bd520d1fecbd380064abd29b5f626f275a20a
Skyler Hektner <skylerhektner@gmail.com>
2018-01-26 20:33:57 -0700

    Created Blob prefab and added movement

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour {
+
+    public Transform objectToFollow;
+    public bool preserveOffset = false;
+
+    private Vector3 offset;
+
+	void Start () {
+		if (preserveOffset)
+        {
+            offset = transform.localPosition - objectToFollow.localPosition;
+        }
+        else
+        {
+            offset = new Vector3(0, 0, transform.localPosition.z);
+        }
+	}
+	
+	void Update ()
+    {
+        transform.localPosition = objectToFollow.localPosition + offset;
+	}
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 10f;
+    public bool movementAllowed = true;
+
+	void Update ()
+    {
+        if(!movementAllowed)
+        {
+            return;
+        }
+
+        Debug.Log(Input.GetAxis("Horizontal") + " : " + Input.GetAxis("Vertical"));
+
+        transform.localPosition += new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0);
+        transform.localPosition += new Vector3(0, Input.GetAxis("Vertical") * speed, 0);
+    }
+}

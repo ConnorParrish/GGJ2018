306a09beb9175ff30dee97285c213dc6de8a7326
Heimdyll <real.heimdyll@gmail.com>
2018-01-27 16:11:41 -0700

    Actually added enhanced camera

---
+++ Assets/CrosshairMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairMovement : MonoBehaviour {
+    public float speed;
+    private bool usingController;
+    private Camera mainCamera;
+    private Vector3 nextPos;
+    public int resetCooldown;
+    public int maxRadius;
+
+    // Use this for initialization
+    void Start () {
+        usingController = Input.GetJoystickNames().Length == 1 && Input.GetJoystickNames()[0] != "";
+
+        Debug.Log((usingController ? "Using: " + Input.GetJoystickNames()[0] : "Using: Mouse"));
+
+        mainCamera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (resetCooldown < 1)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, Vector3.zero, .1f);
+            resetCooldown = 0;
+        }
+        
+        if (!usingController)
+        {
+            resetCooldown = (Input.GetAxis("Mouse X") == 0 && Input.GetAxis("Mouse Y") == 0) ? resetCooldown - 1 : 50;
+
+            Vector3 pos = Input.mousePosition;
+            nextPos = mainCamera.ScreenToWorldPoint(pos);//new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        }
+        else
+        {
+            resetCooldown = (Input.GetAxis("RightJoystick X") == 0 && Input.GetAxis("RightJoystick Y") == 0) ? resetCooldown - 1 : 50;
+
+            nextPos.x = Input.GetAxis("RightJoystick X") * speed * Time.deltaTime;
+            nextPos.y = Input.GetAxis("RightJoystick Y") * speed * Time.deltaTime;
+        }
+
+        if (Mathf.Abs((transform.localPosition + nextPos).x) > 15)
+            nextPos.x = 0;
+        if (Mathf.Abs((transform.localPosition + nextPos).y) > 8)
+            nextPos.y = 0;
+
+        transform.localPosition += nextPos;
+    }
+}
---
+++ Assets/Scripts/Mutagens/Mutagen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mutagen : MonoBehaviour {
+
+    public GameObject Player;
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

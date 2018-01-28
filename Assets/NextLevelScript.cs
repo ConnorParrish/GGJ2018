f8b0e155db41ea069212615562fe8402a7bcb2c1
Heimdyll <real.heimdyll@gmail.com>
2018-01-28 12:46:56 -0700

    Added level progression

---
+++ Assets/NextLevelScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevelScript : MonoBehaviour {
+
+    public int nextSceneID;
+    public GameObject nextLevelScreen;
+
+    void Update()
+    {
+        if (nextLevelScreen.activeSelf && Input.GetAxisRaw("Submit") == 1)
+            LoadNextScene();
+    }
+
+	public void LoadNextScene()
+    {
+        Application.LoadLevel(nextSceneID);
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            nextLevelScreen.SetActive(true);
+    }
+}

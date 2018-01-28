0778756943bacde74935f64b5e8185d56aa51ae6
ConnorParrish <heimdyll@outlook.com>
2018-01-28 00:25:40 -0700

    Continued work on mutagen UI

---
+++ Assets/Scripts/Mutagens/Sneeze.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sneeze : MonoBehaviour {
+    Animator animController;
+    private bool hasSneezed;
+
+    private void Start()
+    {
+        animController = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetAxisRaw("Sneeze") == 1 && hasSneezed == false)
+        {
+            hasSneezed = true;
+            animController.SetTrigger("sneeze");
+        }
+	}
+}
